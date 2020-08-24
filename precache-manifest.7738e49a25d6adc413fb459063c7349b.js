44d0735e2be1db5e0431c153fb62a037b7496e4a
Oleksandr Dashevskyi <dashevskyi2013@gmail.com>
2020-08-24 00:02:02 -0700

    Updates

---
+++ precache-manifest.7738e49a25d6adc413fb459063c7349b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be0d1ba36f4515147d641dba0fffa7d",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "41dfef7c0e91cdbcf39f",
+    "url": "/kanban/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "77c1a4033a3be8908d16",
+    "url": "/kanban/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "41dfef7c0e91cdbcf39f",
+    "url": "/kanban/static/js/2.c23843e0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kanban/static/js/2.c23843e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c1a4033a3be8908d16",
+    "url": "/kanban/static/js/main.572d41b2.chunk.js"
+  },
+  {
+    "revision": "f5aa037a105c127e43db",
+    "url": "/kanban/static/js/runtime-main.72bc9640.js"
+  }
+]);
